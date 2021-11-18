76bec736e69c178cc83d46bed8eaa1a0be9e4bff
HarisSaran <hsaran100@academic.rrc.ca>
2021-11-18 13:53:05 -0600

    added index, alphabetized missing email action views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,9 @@
 class CustomersController < ApplicationController
   def index
+    @customers = Customer.includes(:customer).all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :customers, onlu: %i[index show]
+  resources :customers, only: %i[index show]
 end
